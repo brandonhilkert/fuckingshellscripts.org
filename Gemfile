76187392ea5691be79fe1d65699ff1d2e6955da1
Brandon Hilkert <brandonhilkert@gmail.com>
2013-08-17 16:04:27 -0400

    Add assetpack

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 
 gem 'sinatra'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
 gem 'puma'
--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'bundler'
 Bundler.require
 
+assets {
+  js :application, 'js/application.js', [
+    '/js/generic.js',
+    '/js/rainbow.min.js',
+    '/js/shell.js'
+  ]
+
+  css :application, '/css/application.css', [
+    '/css/github.css',
+    '/css/fss.css'
+  ]
+}
+
 get '/' do
   erb :index
 end
